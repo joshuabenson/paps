f3e0bfae27a8e6c04aa0d0c6681787d2c08206cf
Joshua Benson <bensonjosha@gmail.com>
2017-07-04 18:09:30 -0500

    fix server port

--- server/server.js
+++ server/server.js
@@ -3,3 +3,6 @@ var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, '../')));
 // app.use('/', express.static('__dirname'));
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log("Listening on port " + port);
