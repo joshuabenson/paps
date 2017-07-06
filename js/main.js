9b2598211917de94a1e34f971405d259b573e24e
Joshua Benson <bensonjosha@gmail.com>
2017-07-06 02:46:13 -0500

    lots of new stuff

--- js/main.js
+++ js/main.js
@@ -18,10 +18,10 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    .when("/calendar", {templateUrl: "partials/calendar.html", controller: "PageCtrl"})
+    // .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
     .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    // .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
     // Blog
     // .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
@@ -34,15 +34,21 @@ app.config(['$routeProvider', function ($routeProvider) {
  * Controls the Blog
  */
 app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
 });
 
 /**
  * Controls all other Pages
  */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
-
+app.controller('PageCtrl', function ( $window, $scope ) {
+  $scope.nightLevel = '1';
+  $window.addEventListener('scroll', function() {
+      $scope.nightLevel = $window.innerHeight > $window.scrollY ? (1 - $window.scrollY / $window.innerHeight) : 0;
+      $scope.$apply(function(){
+          $scope.nightLevel = $scope.nightLevel + '';
+      });
+      
+      
+  });
   // Activates the Carousel
   $('.carousel').carousel({
     interval: 5000
