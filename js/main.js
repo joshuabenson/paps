9621fa7a691a0cee2b7f2f944c488f079b260f89
Joshua Benson <bensonjosha@gmail.com>
2017-07-13 00:47:07 -0500

    lots of shit man

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
-    .when("/calendar", {templateUrl: "partials/calendar.html", controller: "PageCtrl"})
+    .when("/calendar", {templateUrl: "partials/calendar.html", controller: "ScheduleCtrl"})
     // .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
     .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
     // .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
@@ -60,3 +60,79 @@ app.controller('PageCtrl', function ( $window, $scope ) {
     selector: "a[data-toggle=tooltip]"
   })
 });
+
+app.controller('ScheduleCtrl', function ( $window, $scope ) {
+  
+
+  //responsiveTable ('table');
+  //responsiveTable ('.table-class');
+  responsiveTable('#table-id');
+
+
+  balanceSize();  // on load get balance on size if the browser has small screen
+  
+
+// get responsive table for small screens 
+
+/*  the concept for that tool
+* if the screen width lt 1000 the table hide the table ,
+* and for each td in table row append two bootstrap div col-xs-6 one for td and the other for the opposite th 
+*
+* Now You have the concept so you can develop your own code :) and control on xs divs view
+*/
+  function responsiveTable (table){
+
+      var RowNum = 0;                   // to append row number before each data
+
+      $(table+' tr:gt(0)').each(function(){       // select all table rows except table header row
+
+        // append the header for row number to the small table 
+        $('.table-xs').append('<div class="row-header"> <h5> Session '+RowNum+'</h5> </div>');
+
+        $(this).find('td').each(function(){   // select all table data 
+
+          // for each td get it's equivalent header and append header text to the small table header div
+          $('.table-xs').append('<div class="col-xs-6 header">'+$('th').eq($(this).index()).text()+'</div>');
+        
+          // for each td get it's text and append it to the small table data div
+          $('.table-xs').append('<div class="col-xs-6 data">'+$(this).text()+'</div>');
+      
+        });
+        
+        RowNum++;
+      
+      });
+
+    }
+
+    balanceSize();
+
+  // IMPORTANT on resize fun will be excessive load on your processor if you have many rows 
+
+  $(window).resize(function(){      
+    if ($(window).width() < 1000 ){
+      balanceSize(); // balance the size on resize 
+    }
+  });
+
+
+  
+    // balance size will make all headers height equal data height !!! Just to fill the BG color ><
+
+  function balanceSize(){
+    var i = 0 ;   
+    $('.header').each(function(){
+
+      $(this).height($('.data').eq(i).height());
+
+      i++;
+    });
+  }
+
+
+});
+
+
+/*
+* Developed By Yasser Mas 
+*/
